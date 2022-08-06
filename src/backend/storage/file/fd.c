5862c7e9682674c9c3cd13c557f7a8162e4e93e1
Thomas Munro <tmunro@postgresql.org>
2022-08-06 12:02:43 +1200

    Replace pgwin32_is_junction() with lstat().
    Now that lstat() reports junction points with S_IFLNK/S_ISLINK(), and
    unlink() can unlink them, there is no need for conditional code for
    Windows in a few places.  That was expressed by testing for WIN32 or
    S_ISLNK, which we can now constant-fold.
    
    The coding around pgwin32_is_junction() was a bit suspect anyway, as we
    never checked for errors, and we also know that errors can be spuriously
    reported because of transient sharing violations on this OS.  The
    lstat()-based code has handling for that.
    
    This also reverts 4fc6b6ee on master only.  That was done because
    lstat() didn't previously work for symlinks (junction points), but now
    it does.
    
    Tested-by: Andrew Dunstan <andrew@dunslane.net>
    Discussion: https://postgr.es/m/CA%2BhUKGLfOOeyZpm5ByVcAt7x5Pn-%3DxGRNCvgiUPVVzjFLtnY0w%40mail.gmail.com
    (cherry picked from commit 5fc88c5d53e43fa7dcea93499d230a0bf70f4f77)

--- src/backend/storage/file/fd.c
+++ src/backend/storage/file/fd.c
@@ -3288,7 +3288,6 @@ SyncDataDirectory(void)
 	 */
 	xlog_is_symlink = false;
 
-#ifndef WIN32
 	{
 		struct stat st;
 
@@ -3300,10 +3299,6 @@ SyncDataDirectory(void)
 		else if (S_ISLNK(st.st_mode))
 			xlog_is_symlink = true;
 	}
-#else
-	if (pgwin32_is_junction("pg_wal"))
-		xlog_is_symlink = true;
-#endif
 
 	/*
 	 * If possible, hint to the kernel that we're soon going to fsync the data
