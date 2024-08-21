870f9ca2f33427425b2fb36e3ffe4f0fb573c320
Mark Dilger <mark.dilger@enterprisedb.com>
2024-08-20 20:07:35 -0700

    Stop calling directly into _bt_getrootheight()
    Instead, call through a function pointer in the amapi.  This makes
    it easier for other indexes (including xtree) to hook into the
    optimizer at this point.

--- src/include/access/amapi.h
+++ src/include/access/amapi.h
@@ -140,6 +140,9 @@ typedef void (*amcostestimate_function) (struct PlannerInfo *root,
 										 double *indexCorrelation,
 										 double *indexPages);
 
+/* estimate height of a tree-structured index */
+typedef int (*amgetrootheight_function) (Relation rel);
+
 /* parse index reloptions */
 typedef bytea *(*amoptions_function) (Datum reloptions,
 									  bool validate);
@@ -272,6 +275,7 @@ typedef struct IndexAmRoutine
 	amvacuumcleanup_function amvacuumcleanup;
 	amcanreturn_function amcanreturn;	/* can be NULL */
 	amcostestimate_function amcostestimate;
+	amgetrootheight_function amgetrootheight;	/* can be NULL */
 	amoptions_function amoptions;
 	amproperty_function amproperty; /* can be NULL */
 	ambuildphasename_function ambuildphasename; /* can be NULL */
